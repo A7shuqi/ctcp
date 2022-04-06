acaa81be843f97752324524c3098cb292bcdc1f2
zk <zk@zk.com>
2022-04-06 17:28:58 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zk/project/ctcp
+# Build directory: /home/zk/project/ctcp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file main.cpp
+ * @author zk (zk@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+}
